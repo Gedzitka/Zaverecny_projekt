76d89f4326b3b11135b5772142aaaf95f57976e1
Jana Dvorakova <dvorakova.jana.vf@gmail.com>
2023-01-06 22:54:48 +0100

    yes

---
+++ script.js
@@ -0,0 +1,11 @@
+// nav menu
+const hamburger = document.querySelector(".header__hamburger");
+const navMenu = document.querySelector(".header__nav-menu");
+hamburger.addEventListener("click",function (){
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  })
+  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", function (){
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }))
